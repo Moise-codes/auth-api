411231f2c96d7bdfaba2ab385cf13db5d3d07a9b
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-25 17:03:59 +0200

    Fix bugs and commit

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword)
+    return res.status(400).json({ message: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+require("dotenv").config();
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use("api/auth",authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Auth API running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
